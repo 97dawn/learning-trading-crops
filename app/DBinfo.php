2c96799fb87db73dd5a7543a2388d33967d45de1
Doeun Kim <graceee318@gmail.com>
2018-06-14 14:38:52 +0900

    Update DBinfo.php

--- app/DBinfo.php
+++ app/DBinfo.php
@@ -5,6 +5,7 @@
     $un = 'root';
     $pw = 'root';
 /*
+comment upper block of code and uncomment this if you want to use https://thegreatestfarmer.000webhostapp.com/
     $hn = 'localhost';
     $db = 'id5970972_fpdb';
     $un = 'id5970972_baodoyoo';
